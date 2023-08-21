018a12d0ad4b910e521f4dc2692abba0b354500f
mathhmz <rm551843@fiap.com.br>
2023-08-21 12:49:19 -0300

    Ajuste na incosistencia do request de categories

--- pages/categories/delete/[...id].js
+++ pages/categories/delete/[...id].js
@@ -17,10 +17,12 @@ export default function DeleteCategoryPage(){
         }
 
         axios.get('/api/categories?id=' +id).then(response => {
-            setCategoryInfo(response.data);
+            setCategoryInfo(response?.data);
         })
         
     }, [id])
+
+
 
     function goBack(){
         router.push('/categories')
@@ -38,7 +40,7 @@ export default function DeleteCategoryPage(){
         <Layout>
 
             <div>
-                <h1 className="text-center">Voce realmente deseja deletar a categoria"<span className="text-red-500">{CategoryInfo?.name}</span>" ?</h1>
+                <h1 className="text-center">Voce realmente deseja deletar a categoria?</h1>
                 <div className="flex justify-between">
                     <button
                     onClick={deleteCategory}
